d1d08ac94d94c7a771a810328dde66c3dbce6402
Raymond Gigliotti <ray@firestitch.com>
2024-10-11 09:22:08 -0400

    removed common module

--- src/app/fs-scrollbar.module.ts
+++ src/app/fs-scrollbar.module.ts
@@ -1,13 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { FsScrollbarService } from './services/scrollbar.service';
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-  ],
   providers: [
     FsScrollbarService,
   ],
